7e45d1e0f95a679a47b7fee87c39d44ecfe5d252
jha-saumya <jha.saumya23@gmail.com>
2022-02-03 11:39:44 -0800

    Second Commit

--- Todo.js
+++ Todo.js
@@ -1,11 +1,19 @@
 
+ 
 
-add= document.getElementById("add");
-add.addEventListerner("click", function ()
-{
-    console.log("Submission Completed");
-});
+  document.getElementById("add").addEventListener("click", function() {
+   
+    var user= document.getElementById("TaskName").value ;
+    console.log(user);
+    console.log(document.getElementById("Duedate").value);
+    console.log(document.getElementById("Status").value);
+    
+    console.log("Submission Complete") ; 
+   }); 
 
+    
+   
+ 
 
 
 
